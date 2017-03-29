c662adb8bd87fe992990ddc9e6f615965aa0c3a9
frankmugo <frankmugo@yahoo.com>
2017-03-29 11:26:32 -0400

    initial commit of files

---
+++ changer_maker.rb
@@ -0,0 +1,22 @@
+def parse_change(num)
+
+	num = num.to_s
+
+	num[num.index('.') + 1,num.length].to_i
+end
+
+def change(amount)
+
+	available_coins = {quarter: 25, dime: 10, nickel: 5, penny: 1}; coins = {}
+
+	available_coins.each do |coin, value|
+
+if amount/value > 0
+
+	coins[coin] = 0
+    		(amount/value).times{ coins[coin] += 1; amount -= value }
+    	end
+	end
+
+	coins
+end
---
+++ changer_maker_test.rb
@@ -0,0 +1,24 @@
+require "minitest/autorun"
+require_relative "changer_maker.rb"
+
+class TestChangeMaker < Minitest::Test 
+
+	$change_of_11 = {:dime => 1, :penny => 1}
+	$change_of_93 = {:quarter => 3, :dime => 1, :nickel => 1, :penny => 3}
+	$change_of_31 = {:quarter => 1, :nickel => 1, :penny => 1}
+
+	def test_returns_change
+
+		assert_equal(11, parse_change(1.11))
+end
+
+def test_change
+
+		results = change(11)
+		assert_equal($change_of_11, results)
+		results = change(93)
+		assert_equal($change_of_93, results)
+		results = change(31)
+		assert_equal($change_of_31, results)
+	end
+end
